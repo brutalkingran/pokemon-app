5b2177fd509104c9f7cfe4184df093bb326314e7
brutalkingran <swkvideogames@gmail.com>
2025-07-30 20:37:16 -0300

    axios implementado

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from 'react';
 import PokemonCard from './PokemonCard.jsx';
+import axios from 'axios';
 
 const PokemonList = () => {
-  
+  const [pokemonData, setPokemonData] = useState( [] );
+  const [loading, setLoading] = useState( false );
+  const [error, setError] = useState( null );
+
+  const fetchPokeApi = async () => {
+    setLoading( true );
+    setError( null );
+
+    try {
+      const { data } = await axios.get(`${import.meta.env.BASE_URL}pokemon/ditto`);
+      
+      setPokemonData( [... data] );
+      console.log(pokemonData);
+      
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokeApi();
+  }, [])
 
   return (
     <div className='flex m-2'>
-      <PokemonCard/>
+      { loading && <p>Cargando...</p> }
+
+      {
+        pokemonData.map((pokemon) => <PokemonCard data = {pokemon} key={pokemon.id}/>)
+      }
+
+      { error && <p>Error: {error} </p> }
     </div>
   )
 }
 
-export default PokemonList
+export default PokemonList;
